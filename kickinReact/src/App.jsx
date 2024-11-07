4fb2229c3aed10b7b1275744186ba8b498f387b4
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-07 17:30:08 +0530

    netflix basics

--- kickinReact/src/App.jsx
+++ kickinReact/src/App.jsx
@@ -1,12 +1,31 @@
 export default function App(){
   return (
-    <div className="container">
-        <img src="img/poster.jpg" alt="img/poster.jpg" height="40%" width="50%"/>
+    <>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    <NetFlix/>
+    </>
+  )
+}
+
+const NetFlix = () =>{
+  return(
+  <>
+      <div className="container">
+        <img src="img/poster.jpg" alt="img/poster.jpg" height="30%" width="40%"/>
         <div>
           <h3>Name: DareDevil</h3>
           <h4>Rating : 8.2</h4>
           <p>Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.</p>
         </div>
     </div>
+  </>
   )
 }
