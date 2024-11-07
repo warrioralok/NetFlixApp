f578175d410c9103cd6a5a26d52104bf36500857
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-07 23:23:16 +0530

    age condition added

--- kickinReact/src/App.jsx
+++ kickinReact/src/App.jsx
@@ -21,7 +21,7 @@ const NetFlix = () =>{
   let name = 'DareDevil';
   let rating = '8.2';
   let desc = 'Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.';
-  let age = 15;
+  let age = 18;
   return(
   <>
       <div className="container">
@@ -31,6 +31,7 @@ const NetFlix = () =>{
           <h4>Rating : {rating}</h4>
           <p>{desc}</p>
           <p>Category : { age >= 18 ? 'A' : 'U'}</p>
+          { age >= 18  && <div className="watchButton">Watch Now</div> }
         </div>
     </div>
   </>
