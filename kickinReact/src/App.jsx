e905cc127e1a4d0156c7988f79d52c89372acaa3
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-07 16:51:50 +0530

    movie poster

--- kickinReact/src/App.jsx
+++ kickinReact/src/App.jsx
@@ -1,3 +1,12 @@
-export const App = () => {
-  return <h1>React Kickin !!!</h1>
+export default function App(){
+  return (
+    <div className="container">
+        <img src="img/poster.jpg" alt="img/poster.jpg" height="40%" width="50%"/>
+        <div>
+          <h3>Name: DareDevil</h3>
+          <h4>Rating : 8.2</h4>
+          <p>Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.</p>
+        </div>
+    </div>
+  )
 }
