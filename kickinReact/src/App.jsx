f2a0e54bc00e85d10567af2c4ad412fb8aa99a92
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-07 20:59:52 +0530

    netflix card

--- kickinReact/src/App.jsx
+++ kickinReact/src/App.jsx
@@ -1,6 +1,7 @@
 export default function App(){
   return (
     <>
+    <div className="parent">
     <NetFlix/>
     <NetFlix/>
     <NetFlix/>
@@ -11,19 +12,25 @@ export default function App(){
     <NetFlix/>
     <NetFlix/>
     <NetFlix/>
+    </div>
     </>
   )
 }
 
 const NetFlix = () =>{
+  let name = 'DareDevil';
+  let rating = '8.2';
+  let desc = 'Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.';
+  let age = 15;
   return(
   <>
       <div className="container">
         <img src="img/poster.jpg" alt="img/poster.jpg" height="30%" width="40%"/>
         <div>
-          <h3>Name: DareDevil</h3>
-          <h4>Rating : 8.2</h4>
-          <p>Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.</p>
+          <h3>Name: {name}</h3>
+          <h4>Rating : {rating}</h4>
+          <p>{desc}</p>
+          <p>Category : { age >= 18 ? 'A' : 'U'}</p>
         </div>
     </div>
   </>
