0cf1e97de4007bd803cf96be8e35e515c538a8e1
warrioralok <34855411+warrioralok@users.noreply.github.com>
2024-11-09 00:47:38 +0530

    netflix links added dynamically

--- kickinReact/src/components/NetFlix.jsx
+++ kickinReact/src/components/NetFlix.jsx
@@ -1,20 +1,26 @@
+import seriesData from '../api/seriesData.json';
+
 const NetFlix = () =>{
-  let name = 'DareDevil';
-  let rating = '8.2';
-  let desc = 'Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.';
-  let age = 18;
+//   let name = 'DareDevil';
+//   let rating = '8.2';
+//   let desc = 'Daredevil is the Man Without Fear! Blinded as a youngster by radioactive chemicals, Matt Murdock soon found that his other senses had been heightened to extraordinary levels.';
+//   let age = 18;
   return(
   <>
-      <div className="container">
-        <img src="img/poster.jpg" alt="img/poster.jpg" height="30%" width="40%"/>
+  {seriesData.map((index) =>{
+      return <div className="container" key={index.name}>
+        <img src={index.imageUrl} alt="img/poster.jpg" height="30%" width="40%"/>
         <div>
-          <h3>Name: {name}</h3>
-          <h4>Rating : {rating}</h4>
-          <p>{desc}</p>
-          <p>Category : { age >= 18 ? 'A' : 'U'}</p>
-          { age >= 18  && <div className="watchButton">Watch Now</div> }
+          <h3>Name: {index.name}</h3>
+          <h4>Rating : {index.rating}</h4>
+          <p className='des'>{index.description}</p>
+          <p>Cast: {index.cast}</p>
+          <p>Category : {index.genre}</p>
+          {/* { age >= 18  && <div className="watchButton">Watch Now</div> } */}
+          <a href={index.watchUrl} target='_blank'><div className="watchButton">Watch Now</div></a>
         </div>
     </div>
+    })}
   </>
   )
 }
